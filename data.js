1e14c5e7fe957be12cab8901806ebe170705ec8e
Hinase <112501342+HinaseDev@users.noreply.github.com>
2023-01-22 13:13:00 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+/*
+Throw all the Stations in here
+*/
+
+stations = [
+    {
+        name: "UpBeat",
+        description: "UpBeat is a non-profit international radio which strives to provide non-stop, quality radio and news entertainment.",
+        link: "https://upbeatradio.net/"
+    },
+    {
+        name: "Flow Radio",
+        description: "We are Flow, a team of over 20+ DJs! Make sure to join the team today via our discord!",
+        link: "https://itsflow.net"
+    }
+]
